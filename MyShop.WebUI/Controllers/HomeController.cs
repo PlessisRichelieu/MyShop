752bf4b1432bda2e34f2a6b108e918bd3136f4fa
Nick Nickolov <nicknickolov@hotmail.com>
2018-12-01 08:50:02 +0100

    10.Added Product Filters

--- MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop.WebUI/Controllers/HomeController.cs
@@ -3,14 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+using MyShop.Core.ViewModels;
+using MyShop.Core.Contracts;
+using System.IO;
+
 
 namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        IRepository<Product> context;
+        IRepository<ProductCategory> ProductCategories;
+
+        public HomeController(IRepository<Product> ProductContext, IRepository<ProductCategory> ProductCategoryContext)
         {
-            return View();
+            context = ProductContext;
+            ProductCategories = ProductCategoryContext;
+        }
+        public ActionResult Index(string Category = null)
+        {
+            List<Product> products;
+            List<ProductCategory> categories = ProductCategories.Collection().ToList();
+
+            if (Category == null)
+            {
+              products =  context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.category == Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+            return View(model);
+        }
+
+        public ActionResult Details (string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
